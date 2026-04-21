a0202310fd912b7a00c59e140e8902f1f061693e
Ahmed-Devx <ahmedoffice69@gmail.com>
2026-04-22 01:27:12 +0500

    Update layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Sidebar from "@/app/components/Sidebar";
+import { Suspense } from "react"; //  
 
 export const metadata: Metadata = {
   title: "SkyCast Pro | AhmedDevx07",
@@ -15,7 +16,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-[#0a0f1a] text-white min-h-screen custom-scrollbar overflow-x-hidden">
-        <Sidebar />
+        
+        
+        <Suspense fallback={
+          <aside className="fixed left-0 top-0 h-screen w-72 bg-[#0a0f1a] border-r border-white/5 hidden lg:block" />
+        }>
+          <Sidebar />
+        </Suspense>
 
         <main className="flex-1 ml-0 lg:ml-72 min-h-screen transition-all duration-300 ease-in-out pt-20 lg:pt-0 px-4 md:px-8 lg:px-10">
           <div className="max-w-[1600px] mx-auto w-full py-6 md:py-10">
